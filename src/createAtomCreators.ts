de29675215ed7e1d265013ba632b24e10acf004a
Sachin Raja <sachinraja2349@gmail.com>
2022-08-25 14:29:36 -0700

    fix types

--- src/createAtomCreators.ts
+++ src/createAtomCreators.ts
@@ -1,6 +1,6 @@
-import { createTRPCClient } from '@trpc/client';
+import { createTRPCClient, TRPCRequestOptions } from '@trpc/client';
 import type { CreateTRPCClientOptions } from '@trpc/client';
-import type { AnyRouter } from '@trpc/server';
+import type { AnyRouter, inferHandlerInput } from '@trpc/server';
 
 import { atom } from 'jotai';
 import type { Getter } from 'jotai';
@@ -8,15 +8,23 @@ import type { Getter } from 'jotai';
 const isGetter = <T>(v: T | ((get: Getter) => T)): v is (get: Getter) => T =>
   typeof v === 'function';
 
+type ArgsOrGetter<T> = T | ((get: Getter) => T);
+
 export function createAtomCreators<TRouter extends AnyRouter>(
   opts: CreateTRPCClientOptions<TRouter>,
 ) {
   const client = createTRPCClient<TRouter>(opts);
 
-  type Args = Parameters<typeof client.query>;
-
-  const atomWithQuery = (
-    getArgs: Args | ((get: Getter) => Args),
+  type TQueries = TRouter['_def']['queries'];
+  const atomWithQuery = <
+    TPath extends keyof TRouter['_def']['queries'] & string,
+  >(
+    getArgs: ArgsOrGetter<
+      [
+        path: TPath,
+        ...args: [...inferHandlerInput<TQueries[TPath]>, TRPCRequestOptions?],
+      ]
+    >,
     getClient?: (get: Getter) => typeof client,
   ) => {
     const queryAtom = atom(async (get) => {
